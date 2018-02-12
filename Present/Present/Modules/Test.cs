bcf4bf1743f2e05ec12cbb9a9d7b228a44a7b15d
Diggity68 <35615572+Diggity68@users.noreply.github.com>
2018-02-11 18:47:53 -0800

    First upload to Shant Branch

---
+++ Present/Present/Modules/Test.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Present.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("Test")]
+        public async Task something()
+        {
+            await Context.Channel.SendMessageAsync("YOU DID IT!!! I'M ALIIIIIIIIVE!!!!!");
+        }
+
+        [Command("flip")]
+        public async Task flip()
+        {
+            await Context.Channel.SendMessageAsync("WHEEEEEEEEEE!!! *Flips*");
+        }
+
+        [Command("sayhi")]
+        public async Task greet(IGuildUser user)
+        {
+            await Context.Channel.SendMessageAsync("Hi!");
+        }
+    }
+}
---
+++ Present/Present/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace Present
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "NDEyMzI3ODI5MDc0NjA4MTM5.DWJzVQ.II5XOeTAtPiMUYJ3Fw1SCNPwzaU");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            await Task.Delay(-1);
+        }
+
+    }
+}
